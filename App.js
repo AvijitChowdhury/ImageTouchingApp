672867e9d181a4972c9cd9f91ceab299aac10ab8
Avijit Chowdhury <abhichy30@gmail.com>
2022-05-01 10:27:15 +0600

    image touching app:

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Alert, Button, Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+
 
 export default function App() {
+  // let x=1;
+  // console.log('avijt');
+  const handlePress =()=>{
+    console.log('test pressed...');
+  }
+  const [touchCount,setTouchCount]=useState(0);
+  const handleImagePressed=()=>{
+    setTouchCount(touchCount+1);
+  }
+  const handleRefresh= ()=>{
+    setTouchCount(0);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1} onPress={handlePress} style={{paddingBottom:10,fontSize:20}}>Hello Avijit! Welcome to this App. </Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={handleImagePressed}>
+        <Image 
+          accessibilityLabel='none'
+          source={{
+            width:200,
+            height:300,
+            uri:'https://i.picsum.photos/id/0/5616/3744.jpg?hmac=3GAAioiQziMGEtLbfrdbcoenXoWAW-zlyEAMkfEdBzQ'}}
+          ></Image>
+          </TouchableOpacity>
+          <Text style={styles.imageStyle}>Image touched {touchCount} times</Text>
+          <Button
+            onPress={()=>{handleRefresh();
+            Alert.alert('Feedback','Are you enjoying this app,if yes press ok otherwise no.',[
+              {text:'Yes'},
+              {text:'No'}
+            ])}}
+            title="Click Me to Refresh"
+            color="#f194ff"
+            
+          />
     </View>
   );
 }
@@ -13,8 +48,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop:Platform.OS=='android'?StatusBar.currentHeight:0 ,
   },
+  imageStyle:{
+    fontSize:20,
+    paddingTop:20,
+    paddingBottom:10,
+  }
 });
---
+++ README.md
@@ -0,0 +1,14 @@
+<html>
+<head>
+</head>
+
+<body>
+<h2>If you want to run this project in android ,install expo cli app and click the link bellow.Then scan the QR code and enjoy .....</h2>
+<a href="https://expo.dev/@avijit_chy/DoneWithIt">link for project</a>
+
+<h3>project Image</h3>
+<img src="./photo.png">
+
+</body>
+
+</html>
